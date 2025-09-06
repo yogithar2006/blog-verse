1fe9b8ee58db52dd934dcdd57bf28c5dc57a43b9
yogithar2006 <yogitha.r02@gmail.com>
2025-09-06 14:59:47 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,49 @@
+/*let x=5,y=3
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x**y)
+console.log(x%y)
+//arithematic
+let a=5;
+a+=3
+console.log(a)
+a*=3
+console.log(a)
+a/=5
+console.log(a)
+a%=2
+console.log(a)
+//comparison
+let  z=10,b="15"
+console.log(z==b)
+console.log(z!=b)
+console.log(z===b)
+console.log(z!==b)
+console.log(z>=b)
+console.log(z<=b)
+console.log(z>b)
+
+//logical
+let age=20
+console.log(age>=20 && age<=45)
+console.log(age>20|| age<40)
+console.log(!(age<15))*/
+//string 
+let firstName="yogitha"
+let lastName="rayavarapu"
+let fullName=firstName+" "+lastName;
+console.log(fullName)
+let correctName=`hello ${firstName} ${lastName},welcome to the new world`
+console.log(correctName)
+//ternary operator
+let my_age=19
+let result=my_age>=18?"eligible to vote":"not eligible"
+console.log(result)
+
+//increment and decreament 
+let count=3
+console.log(count++)
+console.log(count)
+console.log(++count)
