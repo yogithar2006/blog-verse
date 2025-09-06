607d26bad4c6311dc337ffca2ee58cdd57999c85
yogithar2006 <yogitha.r02@gmail.com>
2025-09-06 15:56:40 +0530

    function

---
+++ functions.js
@@ -0,0 +1,18 @@
+function greet(){
+    console.log("hello")
+}
+greet()
+
+//arrow function
+const square =(x)=> x*x;
+console.log(square(5))
+
+const subtract =(a,b)=>{
+    return a-b
+}
+console.log(subtract(10,4))
+// function
+function add(x,y){
+    return x+y;
+}
+console.log(add(10,4))
