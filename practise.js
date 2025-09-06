a60d25b27690bb8f06280c0a890942be413545d3
yogithar2006 <yogitha.r02@gmail.com>
2025-09-06 12:33:41 +0530

    data types

--- practise.js
+++ practise.js
@@ -1,1 +1,25 @@
 console.log("yogitha")
+let name ="dev astra"
+console.log(name)
+let num =53
+let islogged=false;
+console.log(islogged)
+let x;
+console.log(x)
+let y=null;
+console.log(y)
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof x)
+console.log(typeof y)
+
+let arr=[1,2,3,4]
+console.log(arr[0])
+console.log(typeof arr)
+ let person={
+    name:"yogitha",
+    age:19
+ }
+ console.log(person.name)
+ console.log(person.age)
+ console.log(typeof person)
